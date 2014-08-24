00a84775c104216a660a53f6b8946ff0682f7342
Wolfenhex <wolfenhex@gmail.com>
2014-08-24 12:30:17 -0500

    Added an action button.

--- engine/controls.js
+++ engine/controls.js
@@ -11,21 +11,30 @@ Controls.OnKeyDown = function(event)
 {
 	switch(event.keyCode)
 	{
+		case 69: // E
+		case 32: // Space
+			this.Set('action', true);
+			break;
+
+		case 65:  // A
 		case 37:  // Left
 		case 100: // Left (num)
 			this.Set('left', true);
 			break;
 
+		case 87:  // W
 		case 38:  // Up
 		case 104: // Up (num)
 			this.Set('up', true);
 			break;
 
+		case 68:  // D
 		case 39:  // Right
 		case 102: // Right (num)
 			this.Set('right', true);
 			break;
 
+		case 83: // S
 		case 40: // Down
 		case 98: // Down (num)
 			this.Set('down', true);
@@ -37,22 +46,32 @@ Controls.OnKeyUp = function(event)
 {
 	switch(event.keyCode)
 	{
+		case 69: // E
+		case 32: // Space
+			this.Set('action', false);
+			break;
+
+		case 65:  // A
 		case 37:  // Left
 		case 100: // Left (num)
 			this.Set('left', false);
 			break;
 
+		case 87:  // W
 		case 38:  // Up
 		case 104: // Up (num)
 			this.Set('up', false);
 			break;
 
+		case 68:  // D
 		case 39:  // Right
 		case 102: // Right (num)
 			this.Set('right', false);
 			break;
 
+		case 83: // S
 		case 40: // Down
+		case 98: // Down (num)
 		case 98: // Down (num)
 			this.Set('down', false);
 			break;
