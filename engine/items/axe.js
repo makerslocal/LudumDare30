c16aa8d5ddd801fe543bcbf25d012a596f4cdf25
Charlaxy <charlaxy@gmail.com>
2014-08-24 19:13:37 -0500

    Started an item system with a couple of things: axe.js and wood.js

---
+++ engine/items/axe.js
@@ -0,0 +1,23 @@
+/*
+* item.axe.js
+*/
+
+Weapon.prototype = new Item();
+Weapon.prototype.constructor = Item;
+
+function Axe()
+{
+	this.Style.Background.Image = 'sprites/spritesheet_use.png';
+	this.Style.Background.Position.X = -432;
+	this.Style.Background.Position.Y = -480;
+
+	this.Render = function(element)
+	{
+		if(!element)
+		{
+			return; // No element to render
+		}
+
+		element.setAttribute('item', 'axe');
+	}
+}
