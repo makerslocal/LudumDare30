763c92dcd200bca35cb7aaaeadc07781db277cc2
Hunter Fuller <hfuller@pixilic.com>
2014-08-25 19:42:18 -0500

    Import zones

--- world/index.php
+++ world/index.php
@@ -5,13 +5,21 @@ if ( array_key_exists("world",$_POST) ) {
 	$dirname = substr(str_replace('.','',microtime(TRUE)), 3);
 	mkdir($dirname);
 	file_put_contents($dirname . '/index.html', $_POST["world"]);
-	echo('http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . '?' . $dirname);
+	die('http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . '?' . $dirname);
 } else if ( count($_REQUEST) > 0 ) {
 	//we tryina fetch, nah mean?
 	$dirname = array_keys($_REQUEST)[0];
 	if(is_dir($dirname)) {
-		echo(file_get_contents($dirname . '/index.html'));
+		$im = file_get_contents($dirname . '/index.html');
 	} else {
-		echo("Couldn't find that world...");
+		die("Couldn't find that world...");
 	}
 }
+
+if (!im) die("Couldn't export that world.");
+?>
+<!DOCTYPE html>
+<html><head><title>Importing...</title></head><body><script language="JavaScript">
+localStorage["ld30zones.import"] = '<?php echo($im); ?>';
+document.location = "../game.html";
+</script></body></html>
