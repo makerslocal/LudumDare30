73c760574b965ed5acd1934acd188d511ec3232f
Charlaxy <charlaxy@gmail.com>
2014-08-25 19:48:20 -0500

    Added dolphin enemy that builds walls. :3

--- engine/entity/dolphin.js
+++ engine/entity/dolphin.js
@@ -6,7 +6,7 @@ function Dolphin()
 
 	this.Style.Background.Image = 'sprites.png';
 
-	this.Style.Background.Position.X = 208;
+	this.Style.Background.Position.X = -208;
 	this.Style.Background.Position.Y = -1520;
 
 	this.OnCycle = function(cycles)
@@ -16,39 +16,47 @@ function Dolphin()
 			return;
 		}
 
-		if(!this.Direction)
+		if(cycles % 6)
 		{
-			return
+			if(!this.Direction)
+			{
+				return
+			}
+
+			var x = this.Direction.X * this.Width + this.X;
+			var y = this.Direction.Y * this.Height + this.Y;
+
+			var entity = new Stone();
+			entity.X = x;
+			entity.Y = y;
+
+			entity.Direction = this.Direction;
+			this.World.Entities.Add(entity);
 		}
-
-		var x = this.Direction.X * this.Width + this.X;
-		var y = this.Direction.Y * this.Height + this.Y;
-
-		var entity = new Stone();
-		entity.X = x;
-		entity.Y = y;
-
-		entity.Direction = this.Direction;
-		this.World.Entities.Add(entity);
 
 		this.Wander();
 	}
 
 	this.Render = function(element)
 	{
-		switch(this.Direction)
+		if(!this.Direction)
+		{
+			return;
+		}
+
+		switch(this.Direction.ID)
 		{
 			case Enums.Directions.Down.ID:
-				this.Style.Background.Position.X = -((Engine.Cycles % 3) << 4);
+				this.Style.Background.Position.X = -224;
 				break;
 			case Enums.Directions.Left.ID:
-				this.Style.Background.Position.X = -(((Engine.Cycles % 3) << 4) + 96)
+				this.Style.Background.Position.X = -240;
 				break;
 			case Enums.Directions.Right.ID:
-				this.Style.Background.Position.X = -(((Engine.Cycles % 3) << 4) + 48);
+				this.Style.Background.Position.X = -256;
 				break;
 			case Enums.Directions.Up.ID:
-				this.Style.Background.Position.X = -(((Engine.Cycles % 3) << 4) + 144);
+				this.Style.Background.Position.X = -272;
 				break;
 		}
 
